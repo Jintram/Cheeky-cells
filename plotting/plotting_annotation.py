c49a187df96fbe94cc5b25c9f7678a1952abcae3
“Martijn <martijn.wehrens@gmail.com>
2025-10-14 16:35:36 +0200

    update

---
+++ plotting/plotting_annotation.py
@@ -0,0 +1,41 @@
+
+
+# %% ################################################################################
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+cm_to_inch = 1/2.54
+
+basedir = '/Users/m.wehrens/Data_UVA/2024_07_fluopi_assay/HUMAN_ANNOTATION/20250328_FLUOPPI_humanseg/'
+basefilename = 'A4_hmKeima_001_tile'
+
+# load the data, ['_transform.npy', '_img.npy', '_img_enhanced.npy',  '_annothuman.npy', '_annothuman_features.npy']
+file_list = \
+    {'transform': '_transform.npy', 
+    'img': '_img.npy', 
+    'img_enhanced': '_img_enhanced.npy',  
+    'annotation': '_annothuman.npy', 
+    'annotation_feat': '_annothuman_features.npy'}
+
+img_data = {}
+for key in file_list.keys():
+    fullfilename = basedir + basefilename + file_list[key]
+    img_data[key] = np.load(fullfilename, allow_pickle=True)
+    
+    
+# Now make a plot
+fig, ax = plt.subplots(1, 3, figsize=(60*cm_to_inch, 20*cm_to_inch))
+ax[0].imshow(img_data['img_enhanced'], cmap='gray')
+ax[0].set_title('Enhanced image')
+ax[1].imshow(img_data['img_enhanced'], cmap='gray')
+ax[1].contour(img_data['annotation'], colors='r', levels=[0.5], linewidths=0.5)
+ax[1].set_title('Human annotation')
+ax[2].imshow(img_data['annotation_feat'], cmap='viridis', vmin=0, vmax=3)
+ax[2].set_title('Annotation\n(features)')
+plt.tight_layout()
+# plt.show()
+
+# Save to pdf
+outputdir_plots = basedir + '/_plots/'
+fig.savefig(outputdir_plots + basefilename + '_overview-extra.pdf', bbox_inches='tight')
--- readme.md
+++ readme.md
@@ -3,3 +3,18 @@
 
 # U-Net image segmentation
 
+The goal  of this repository is to facilitate easy training and application of an U-net to 
+recognize objects in an image. 
+
+It has been tested and optimized for detection of cells, but can also be used for other 
+purposes.
+
+## Workflow description
+
+### Annotating images
+
+For this analysis to work, ground truth images need to be created to train the U-net. 
+
+This requires manual annotation of images, but I've already observed good results with
+using only 11 annoted images (which were subdivided over a training and test set), 
+that were 2195x2195 pixels in size.
